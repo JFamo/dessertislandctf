6b2ed735921cfc9f0cbf5a37cffb79739223ecd1
J. Famous <joshua.famous@gmail.com>
2018-12-19 20:15:59 -0500

    Challenge HTML form

---
+++ bas21921903chaptsadudu23suite12.php
@@ -0,0 +1,38 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<title>TheDessertIsland</title>
+		<link rel="stylesheet" href="style.css">
+		<link href="https://fonts.googleapis.com/css?family=Roboto|Roboto+Condensed|Source+Sans+Pro" rel="stylesheet">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	</head>
+	<body>
+		<a id='top' class='jump'></a>
+		<header>
+			<h1>Sweet Challenge, Bro</h1>
+		</header>
+		<main>
+			<h2><i class="fa fa-birthday-cake"></i> CHALLENGE 1 / 10</h2>
+			<p>
+				One consistent thing about deserts, they're all sweet. I love sweets, but I just can't seem to make a selection...
+			</p>
+			<center>
+			<form method="POST" class="flagform">
+				<input type="hidden" name="fid" value="bas21921903chaptsadudu23suite12">
+				<p>TSA{<input type="text" placeholder="flag">}</p>
+				<input type="submit" class="pinkback" value="Submit">
+			</form>
+			</center>
+		</main>
+	</body>
+	<footer>
+		<span><a class="styledlink" href="https://github.com/Megastardaboss">Joshua Famous</a></span>
+		<span>2018</span>
+	</footer>
+</html>
